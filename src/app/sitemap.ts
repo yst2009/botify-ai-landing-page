5a609b1d0edd42fe80f607ef21eb0f503969c3a3
yst2009 <azzaazz172@gmail.com>
2026-07-02 17:00:49 +0300

    fix: optimize sitemap for google search console by removing anchors

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,24 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
-    },
-    {
-      url: `${baseUrl}/#features`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#how-it-works`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#pricing`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
+    }
   ]
 }
+
